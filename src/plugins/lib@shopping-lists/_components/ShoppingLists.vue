0feb40a7ab7878e9db5246813a610214b6bbd601
marosgall <maros.gall@gmail.com>
2024-04-10 20:51:32 +0200

    Pridanie funkcie na pridavanie listov

--- src/plugins/lib@shopping-lists/_components/ShoppingLists.vue
+++ src/plugins/lib@shopping-lists/_components/ShoppingLists.vue
@@ -12,13 +12,13 @@
 			<a :href="`/shopping-lists/${shoppingList.id}`" @click.prevent="openShoppingListDetail(shoppingList)">
 				{{ shoppingList.title }}
 			</a>
-			<ul class="itemsList">
+			<ul v-if="shoppingList.items?.length" class="itemsList">
 				<li v-for="item in shoppingList.items.slice(0, itemCount)">
-					<div v-if="item.is_checked" class="item-unchecked singleItem">
+					<div v-if="item.is_checked" class="item-checked singleItem">
                         <span>{{ item.name }}</span>
                         <span class="itemValue">{{ item.value }} {{ item.unit }}</span>
                     </div>
-                    <div v-else class="item-checked singleItem">
+                    <div v-else class="item-unchecked singleItem">
                         <span>{{ item.name }}</span>
                         <span class="itemValue">{{ item.value }} {{ item.unit }}</span>                
                     </div>
@@ -28,6 +28,11 @@
                 </li>
 			</ul>
 		</div>
+		<button @click="showNewListForm = true">Add new list</button>
+        <div v-if="showNewListForm">
+            <input type="text" v-model="newListTitle" @keydown.enter="addNewList" placeholder="Enter list title">
+            <!-- <button @click="addNewList">Add</button> -->
+        </div>
 	</template>
 </template>
 
@@ -38,20 +43,47 @@
         data() {
 			return {
 				shoppingLists: null,
-				itemCount: 3
+				itemCount: 3,
+				showNewListForm: false,
+                newListTitle: ''
 			}
 		},
         async mounted() {
-			try {
-				const { data: { data: shoppingLists} } = await axios.get('https://shoppinglist.wezeo.dev/cms/api/v1/shopping-lists')
-				this.shoppingLists = shoppingLists
-                console.log(shoppingLists)
-			} catch (error) {
-				console.error('Error:', error)
-				this.shoppingLists = { error }
-			}
+			this.getShoppingLists()
 		},
 		methods: {
+			async getShoppingLists() {
+				try {
+					const { data: { data: shoppingLists} } = await axios.get('https://shoppinglist.wezeo.dev/cms/api/v1/shopping-lists')
+					this.shoppingLists = shoppingLists
+					console.log(shoppingLists)
+				} catch (error) {
+					console.error('Error:', error)
+					this.shoppingLists = { error }
+				}
+			},
+			async addNewList() {
+				try {
+					const response = await axios.post('https://shoppinglist.wezeo.dev/cms/api/v1/shopping-lists', {
+						title: this.newListTitle,
+						items: []
+					})
+					const newList = response.data.data
+					console.log(response.data)
+
+					if (newList && newList.id) {
+						this.shoppingLists.unshift(newList)
+						this.newListTitle = ''
+						this.showNewListForm = false
+
+						this.$router.push({ name: 'Shopping List - Detail', params: { id: newList.id } })
+					} else {
+						console.error(error)
+					}
+				} catch (error) {
+					console.error('Error:', error)
+				}
+			},
 			openShoppingListDetail({ id }) {
 				this.$router.push({ name: 'Shopping List - Detail', params: { id } })
 			}
@@ -61,10 +93,10 @@
 
 <style>
     .item-checked {
-        text-decoration: none;
+        text-decoration: line-through;
     }
 
     .item-unchecked {
-        text-decoration: line-through;
+        text-decoration: none;
     }
 </style>
